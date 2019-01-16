bd4cda25387c6963b91b6608d5f76e42690e5723
Edward Flores <EdwardVFlores@gmail.com>
2019-01-16 11:47:20 -1000

    Classes starting out

---
+++ src/adventure/story/AdventureStory.java
@@ -0,0 +1,16 @@
+package adventure.story;
+
+/**
+ * @author Edward Flores
+ * @date 1/16/2019
+ */
+public class AdventureStory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
