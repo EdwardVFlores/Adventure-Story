f28631a76401b192179134b5f01e19ae01574bfe
Edward Flores <EdwardVFlores@gmail.com>
2019-01-16 11:57:14 -1000

    Classes starting out

--- src/adventure/story/AdventureStory.java
+++ src/adventure/story/AdventureStory.java
@@ -4,6 +4,7 @@ package adventure.story;
  * @author Edward Flores
  * @date 1/16/2019
  */
+
 public class AdventureStory {
 
     /**
---
+++ src/adventure/story/Archer.java
@@ -0,0 +1,10 @@
+package adventure.story;
+
+/**
+ * @author Edward Flores
+ * @date 1/16/2019
+ */
+
+public class Archer {
+    
+}
