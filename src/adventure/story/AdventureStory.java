b928b5b357e46ddadcdc3010565b9041d261f8d3
Edward Flores <EdwardVFlores@gmail.com>
2019-01-18 18:56:38 -1000

    Made attack command able to be used.

--- src/adventure/story/AdventureStory.java
+++ src/adventure/story/AdventureStory.java
@@ -2,26 +2,38 @@ package adventure.story;
 
 /**
  * @author Edward Flores
- * @date 1/16/2019
+ * @date 1/18/2019
  */
 
 import java.util.Scanner;
 
 public class AdventureStory {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to adventure story where you fight monsters!");
         
         Player player = chooseClass();
+        player.getClass().cast(player);
+        
+        System.out.println("What would you like to do?"
+                +"\n1) Attack"
+                +"\n2) Defend");
+        while(true){
+            String action = input.nextLine();
+            if(action.equals("1"))player.Attack();
+            if(action.equals("2"))player.Defend();
+            
+        }
     }
     public static Player chooseClass(){
         Scanner input = new Scanner(System.in);
-        Player player = new Player();
-        while(!player.classChosen){
+        boolean classChosen = false;
+        while(!classChosen){
             try{
                 System.out.println("Choose your class:");
                 System.out.println("1) View Stats");
@@ -32,7 +44,7 @@ public class AdventureStory {
                 int classNum = Integer.parseInt(input.nextLine());
                 switch(classNum){
                 case 1:
-                    System.out.println("Showing stats");
+                    System.out.println("\nShowing stats\n");
 //                    Write a method to choose which stat to show for each class
 //                        1) Choose which stat to show
 //                        2) Print out the selected stat
@@ -47,12 +59,15 @@ public class AdventureStory {
                     System.out.println("\n" + classNum + " is not an option. \n");
                     break;
                 }
+                
+                classChosen = true;
+                
             }catch(NumberFormatException e){
                 System.out.println("\nThat is not a number!\n");
             }catch(Exception e){
                 e.getMessage();
             }
         }
-        return chooseClass(); //Just in case it returns an empty player.
+        return chooseClass();//Just in case it returns an empty player.
     }
 }
