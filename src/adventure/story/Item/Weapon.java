7ed4e5a9fa881abf5708bb8719e1e68c1977c3bf
Edward Flores <EdwardVFlores@gmail.com>
2019-01-20 08:49:23 -1000

    Added a weapon class for a type of item.

---
+++ src/adventure/story/Item/Weapon.java
@@ -0,0 +1,13 @@
+package adventure.story.Item;
+import adventure.story.*;
+import adventure.story.Item.Broadsword.*;
+
+/**
+ * @author Edward Flores
+ */
+public abstract class Weapon {
+    
+    public Weapon(Broadsword name){
+        
+    }
+}
