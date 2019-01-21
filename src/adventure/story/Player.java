f8889745b9d16a808d9f4ee2a7e124743c9283ee
Edward Flores <EdwardVFlores@gmail.com>
2019-01-21 13:58:14 -1000

    Added an inventory to the player and methods to pick up, remove, and show inventory.

--- src/adventure/story/Player.java
+++ src/adventure/story/Player.java
@@ -1,58 +1,81 @@
 package adventure.story;
 
+import adventure.story.Item.*;
+import java.util.ArrayList;
+
 /**
  * @author Edward Flores
  */
 
-public abstract class Player{
+public abstract class Player {
+
     private final String CLASSNAME;
-    private int health,defense;
-    
-    public Player(String className){
+    private int health, defense;
+    private final ArrayList<Item> INVENTORY = new ArrayList<>();
+
+    public Player(String className) {
         this.CLASSNAME = className;
     }
-    
-    public void setHealth(int health){
+
+    public void setHealth(int health) {
         this.health = health;
     }
-    
-    public void setDefense(int defense){
+
+    public void setDefense(int defense) {
         this.defense = defense;
     }
-    
-    public String getClassName(){
+
+    public String getClassName() {
         return this.CLASSNAME;
     }
-    
-    public int getHealth(){
+
+    public int getHealth() {
         return this.health;
     }
-    
-    public int getDefense(){
+
+    public int getDefense() {
         return this.defense;
     }
 
-    public void changeHealth(int health){
+    public void changeHealth(int health) {
         this.health += health;
     }
-    
-    public void changeDefense(int defense){
+
+    public void changeDefense(int defense) {
         this.defense += defense;
     }
-    
+
     public abstract void Attack();
-    
-    public void Defend(){
+
+    public void Defend() {
         System.out.println("Blocking for " + this.defense + " damage.");
     }
-    
-    public void showStats(){
+
+    public void showInventory() {
+        System.out.println("\n+-- Inventory --+");
+        for (int i = 0; i < INVENTORY.size(); i++) {
+            System.out.println(i + 1 + ") " + INVENTORY.get(i).getItemName());
+        }
+        System.out.println("+---------------+\n");
+
+    }
+
+    public void pickUp(Item item) {
+        System.out.println(item.getItemName() + " has been added to your inventory.");
+        INVENTORY.add(item);
+    }
+
+    public void removeItem(int itemNumber) {
+        System.out.println("Item Removed");
+    }
+
+    public void showStats() {
         System.out.println();
-        System.out.println("+-- "+this.CLASSNAME+" Stats --+");
+        System.out.println("+-- " + this.CLASSNAME + " Stats --+");
         System.out.println(" Health: " + this.getHealth());
         System.out.println(" Defense: " + this.getDefense());
         System.out.print("+------");
-        for (int i=0; i<this.CLASSNAME.length(); i++) {
+        for (int i = 0; i < this.CLASSNAME.length(); i++) {
             System.out.print("-");
         }
         System.out.println("------+");
