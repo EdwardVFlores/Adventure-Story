790e3b7cb9bf1b71b8a251b46256b3fd39b3f6ad
Edward Flores <EdwardVFlores@gmail.com>
2019-01-21 13:59:00 -1000

    Added an item class to make weapons.

---
+++ src/adventure/story/Item/Item.java
@@ -0,0 +1,27 @@
+package adventure.story.Item;
+
+/**
+ * @author Edward Flores
+ */
+public class Item {
+    private String ItemType;
+    private String ItemName;
+
+    public String getItemType() {
+        return ItemType;
+    }
+
+    public void setItemType(String ItemType) {
+        this.ItemType = ItemType;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String ItemName) {
+        this.ItemName = ItemName;
+    }
+    
+    
+}
