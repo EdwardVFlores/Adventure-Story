b42d081c0cb8f25b5e9d898bd52e96dbe582e823
Edward Flores <EdwardVFlores@gmail.com>
2019-01-22 21:03:40 -1000

    Changed values to spawn in correct chance of beginner mobs.

--- src/adventure/story/Monster/Monster.java
+++ src/adventure/story/Monster/Monster.java
@@ -10,14 +10,14 @@ public class Monster {
     BeginnerMob mob;
 
     public Monster() {
-        int randNum = (int) (Math.random() * 50 + 1);
-        if (randNum < 70) {
+        int randNum = (int) (Math.random() * 100 + 1);
+        if (randNum < 75) {
             SewerRat sewerRat = new SewerRat();
             mob = sewerRat;
-        } else if (randNum >= 70 && randNum < 90) {
+        } else if (randNum >= 75 && randNum < 90) {
             WildBoar wildBoar = new WildBoar();
             mob = wildBoar;
-        } else if (randNum >= 90) {
+        } else if (randNum >= 80) {
             DyreWolf dyreWolf = new DyreWolf();
             mob = dyreWolf;
         }
